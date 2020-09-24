93d3f3839f0437cfb16dfeec705aeb6e1fbc0b52
rajpurohityogesh <67944765+rajpurohityogesh@users.noreply.github.com>
2020-09-24 18:02:56 +0530

    Add files via upload

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,12 +4,16 @@ for(i=0; i<cardPointer.length ; i++)
 {
     let ele = cardPointer[i]; 
     let x=document.querySelector('.cardStack');
+    let but = ele.querySelector("a");
     ele.addEventListener('mousedown', function() { 
-      ele.setAttribute("display","block");
+      //ele.setAttribute("display","block");
       x.classList.add("cardPopUp");
     });
     ele.addEventListener('mouseup', function(){
-      ele.setAttribute("display","inline");
+      //ele.getAttribute("display","inline");
       x.classList.remove("cardPopUp");
+    });
+    but.addEventListener('click', function(){
+      alert("button clicked");
     });
 } 
